540ac006ab31c56c7085e488baf7de1a13789232
Kihamda <kuawse580@gmail.com>
2025-08-23 13:23:00 +0900

    スカウト詳細エディタに「変更を保存」ボタンを追加

--- src/app/scoutDetail/edit/editor.tsx
+++ src/app/scoutDetail/edit/editor.tsx
@@ -1,5 +1,6 @@
 import { Scout } from "@/types/scout/scout";
 import React from "react";
+import { Button } from "react-bootstrap";
 
 const ScoutDetailEditor = ({
   scoutData,
@@ -14,6 +15,9 @@ const ScoutDetailEditor = ({
       <h1>Edit Scout Detail</h1>
       <p>Editing scout with ID: {scoutData.id}</p>
       <p>{JSON.stringify(scoutData)}</p>
+      <Button variant="primary" onClick={() => setScoutData(scoutData)}>
+        Save Changes
+      </Button>
     </div>
   );
 };
