a93d8aded9a9d473ecb2c9228935b571d45bbcbf
Kihamda <kuawse580@gmail.com>
2025-08-24 15:21:46 +0900

    PWA設定を追加し、マニフェストファイルを作成

--- vite.config.ts
+++ vite.config.ts
@@ -1,47 +1,9 @@
 import { defineConfig } from "vite";
 
-import { VitePWA } from "vite-plugin-pwa";
 import react from "@vitejs/plugin-react-swc";
 
 export default defineConfig({
-  plugins: [
-    react(),
-    VitePWA({
-      registerType: "autoUpdate",
-      includeAssets: ["favicon.ico", "apple-touch-icon.png"],
-      injectRegister: "auto", //ここの記述
-      manifest: {
-        name: "My Historyアプリ from My History of Scouting",
-        short_name: "My History",
-        description: "ボーイスカウト活動を記録・共有するアプリ",
-        theme_color: "#f8f9fa",
-        icons: [
-          {
-            src: "logos/icon192.png",
-            sizes: "192x192",
-            type: "image/png",
-          },
-          {
-            src: "logos/icon512.png",
-            sizes: "512x512",
-            type: "image/png",
-          },
-          {
-            src: "logos/icon512.png",
-            sizes: "512x512",
-            type: "image/png",
-            purpose: "any",
-          },
-          {
-            src: "logos/icon512.png",
-            sizes: "512x512",
-            type: "image/png",
-            purpose: "maskable",
-          },
-        ],
-      },
-    }),
-  ],
+  plugins: [react()],
   resolve: {
     alias: {
       "@": "/src",
